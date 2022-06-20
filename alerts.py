55974331645fafc70885c3468e90521426e64029
jagadeesh <jagadeeshjaga616@gmail.com>
2022-06-20 12:09:28 +0530

    american development tean

--- alerts.py
+++ alerts.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.select import Select
 
 driver =webdriver.Chrome(service=Service(executable_path="C:\Softwares\chromedriver"))
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-driver.find_element(By.CSS_SELECTOR,"#name").send_keys(validateText)
+driver.find_element(By.CSS_SELECTOR,"#name").send_keys("validateText")
 driver.find_element(By.ID,"alertbtn").click()
 alert = driver.switch_to.alert
 alertText = alert.text
