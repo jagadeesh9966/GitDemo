365011532a7d1bd33a8bb5bfe1040f137e4306c9
jagadeesh <jagadeeshjaga616@gmail.com>
2022-06-20 11:35:22 +0530

    american guy commit

--- actionsDemo.py
+++ actionsDemo.py
@@ -11,11 +11,21 @@ menu = driver.find_element(By.ID,"mousehover")
 action.move_to_element(menu).perform()
 childMenu = driver.find_element(By.LINK_TEXT,"Top")
 action.move_to_element(childMenu).click().perform()'''
-driver.get("https://chercher.tech/practice/practice-pop-ups-selenium-webdriver")
+#1st
+"""driver.get("https://chercher.tech/practice/practice-pop-ups-selenium-webdriver")
 action = ActionChains(driver)
 action.double_click(driver.find_element(By.ID,"double-click")).perform()
 
 alert = driver.switch_to.alert
 assert "You double clicked me!!!, You got to be kidding me" == alert.text
 alert.accept()
-print(jagadeesh)
+print(jagadeesh)"""
+
+#2nd program
+driver.get("http://www.demo.guru99.com/V4/")
+driver.find_element(By.CSS_SELECTOR,"input[name='uid']").send_keys("mngr409462")
+driver.find_element(By.CSS_SELECTOR,"input[name='password']").send_keys("yhUdApA")
+driver.find_element(By.CSS_SELECTOR,"input[type='submit']").click()
+print(driver.title)
+print(driver.current_url)
+driver.save_screenshot("guru"+str(random.randint(0,70))+".png")
--- alerts.py
+++ alerts.py
@@ -1,22 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
-	Having around 3 years of IT industry experience in software Engineering.
-	Experience on automate web application using python selenium web driver.
-	Strong experience in python object oriented programming.
-	Hands on experience in automated GUI test cases using Python script.
-	Hand s -on experience in selenium web driver Using object oriented programming language.
-	Hands on experience on developed scripts using Object Oriented Programming.
-	Knowledge on the Rest API.
-	Experienced in manual execution of test cases in LINUX and Windows environment.
-	Hands on Experience on Bug Life Cycle.
-	Hand s -on experience on Pycharm IDE and GIT.
-	Hand s -on experience on Pytest frameworks.
-	Experience	in Requirements	understanding,	analyzing	and	reporting	issues	on
-Requirements.
-	Experience in Sanity, GUI, Functional, Performance and regression testing concepts.
-	Experience in preparation of Test scenarios, Test cases, Test case reviews, Test
-execution and Defect reporting.
-	Able to work collaboratively with testers, developers, and other team members.
-	Commitment, result oriented and interes
+driver =webdriver.Chrome(service=Service(executable_path="C:\Softwares\chromedriver"))
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.find_element(By.CSS_SELECTOR,"#name").send_keys(validateText)
 driver.find_element(By.ID,"alertbtn").click()
